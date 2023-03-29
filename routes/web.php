5d876a00c283fc53577c8a4e9a7d878ff1b3d125
Bia Verly <biaverly@bia.verly@oliveiratrust.com.br>
2023-03-29 10:07:56 -0300

    Rotas de login

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::controller(LoginController::class)->group(function()
+{   
+    Route::get('/login','index')->name('login');
+    Route::post('/login','login')->name('entrar');
+    Route::get('/logout','destroy')->name('logout');
+    Route::get('/login/registar','create')->name('criarUsuario');
+    Route::post('/login/registar','store')->name('salvarUsuario');
+});
